037f0b42eb1af66b61e03219b04a956aa4315f3f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-10 20:35:10 -0700

    Add comments in index.html.

--- js/app.js
+++ js/app.js
@@ -32,14 +32,14 @@ function iterateResults() {
 
 function displayResults(result) {
   var liEl = document.createElement('li');
-
   var title = result.title;
   var snippet = result.snippet;
   var insertUnderscore = title.replace(' ', '_');
   var pageLink = `https://en.wikipedia.org/wiki/${insertUnderscore}`;
 
   ulEl.appendChild(liEl);
-  liEl.innerHTML = `
+  
+  return liEl.innerHTML = `
     <a href=${pageLink} target="_blank">${title}</a>
     <p>${snippet}</p>
   `
