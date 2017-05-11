1bd9f1a2aca3f53aeddd43d83250575b5eba2525
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-10 20:21:23 -0700

    Create form element in index.html. Populate results on webpage based on searchInput.value.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,46 @@
-var endpoint = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&titles=lakers&srsearch=lakers&srnamespace=-2";
-
-var obj;
-
+var formEl = document.querySelector('form');
+var searchInput = document.querySelector('.searchInput');
 var ulEl = document.querySelector('ul');
+var searchResults;
 
-function foo() {
-  obj.query.search.map(function(item){
-    var liEl = document.createElement('li');
+formEl.addEventListener('submit', cb);
 
-    var title = item.title;
-    var snippet = item.snippet;
-    var insertUnderscore = title.replace(' ', '_');
-    var pageLink = `https://en.wikipedia.org/wiki/${insertUnderscore}`;
+function cb(evt) {
+  evt.preventDefault(); // prevent page from refreshing
 
-    ulEl.appendChild(liEl);
-    liEl.innerHTML = `
-      <a href=${pageLink} target="_blank">${title}</a>
-      <p>${snippet}</p>
-    `
-  } );
+  var endpoint = `https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${searchInput.value}`;
+
+  return ajaxRequest(endpoint)
+    .then( function(val){
+      searchResults = val;
+      console.log(val);
+      displayResults();
+    } )
+    .catch( function(err){
+      console.log(err);
+    } )
+}
+
+function displayResults() {
+  ulEl.innerHTML = ''; // erase any existing content in <ul></ul>
+  return searchResults.query.search.map(domManip)
 
 }
 
-ajaxRequest(endpoint)
-  .then( function(val){
-    obj = val;
-    console.log(val);
-    foo();
-  } )
-  .catch( function(err){
-    console.log(err);
-  } )
+function domManip(item) {
+  var liEl = document.createElement('li');
 
+  var title = item.title;
+  var snippet = item.snippet;
+  var insertUnderscore = title.replace(' ', '_');
+  var pageLink = `https://en.wikipedia.org/wiki/${insertUnderscore}`;
+
+  ulEl.appendChild(liEl);
+  liEl.innerHTML = `
+    <a href=${pageLink} target="_blank">${title}</a>
+    <p>${snippet}</p>
+  `
+}
 
 function ajaxRequest(url) {
   return new Promise( function(resolve, reject){
